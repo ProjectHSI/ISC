6841ace22a7ec5853598982235c503b033659b3d
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2021-12-01 07:56:08 +1000

    g

---
+++ Addresses.lua
@@ -0,0 +1,4 @@
+-- Returns addresses so things can communicate to each other.
+function Addresses()
+    return { MainServer = "" }
+end
---
+++ FileBase.lua
@@ -0,0 +1,8 @@
+-- This is the system desecription here, describing what it does.
+-- Endpoints describe what data to use with IRFC standards.
+-- Endpoints:
+--  
+-- Initalize certain components
+component = component or require("component")
+modem = component.modem
+-- Additional components can be used
